43a0e4753ea50ef38f018a074c2bdfa614848ef9
Giovanni Rossini <giovannijrrossini@gmail.com>
2019-08-13 18:31:25 -0300

    Heroku DB

--- lib/database.go
+++ lib/database.go
@@ -7,12 +7,13 @@ import (
 	"upper.io/db.v3/mysql"
 )
 
-var config = mysql.ConnectionURL{
-	Host:     "us-cdbr-iron-east-03.cleardb.net",
-	User:     "bd51013dec5525@us-cdbr-iron-east-03.cleardb.net",
-	Password: "d1be2d4",
-	Database: "heroku_9f89a4257664aca",
-}
+var config = os.Getenv("DATABASE_URL")
+// var config = mysql.ConnectionURL{
+// 	Host:     "us-cdbr-iron-east-03.cleardb.net",
+// 	User:     "bd51013dec5525@us-cdbr-iron-east-03.cleardb.net",
+// 	Password: "d1be2d4",
+// 	Database: "heroku_9f89a4257664aca",
+// }
 
 // Sess connection var database
 var Sess db.Database
